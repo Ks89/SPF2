7cc75038da3228e71276943b4df27fdeebb9d956
Stefano Cappa <stefano.cappa@mail.polimi.it>
2015-07-05 11:49:27 +0200

    Updated copyright in gradle e WFDMid.

--- sPFWFDMid/src/main/java/it/polimi/spf/wfd/GroupActorListener.java
+++ sPFWFDMid/src/main/java/it/polimi/spf/wfd/GroupActorListener.java
@@ -1,10 +1,11 @@
-/* 
+/*
  * Copyright 2014 Jacopo Aliprandi, Dario Archetti
- * 
+ * Copyright 2015 Stefano Cappa
+ *
  * This file is part of SPF.
- * 
+ *
  * SPF is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free 
+ * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
@@ -12,11 +13,12 @@
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with SPF.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
+
 package it.polimi.spf.wfd;
 
 interface GroupActorListener extends WfdMiddlewareListener {
